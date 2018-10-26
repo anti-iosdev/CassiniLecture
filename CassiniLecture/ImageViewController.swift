cf9d031f65dafa8b9c1fc730740d76726eace14e
Anti <anti@antis-imac-pro.local>
2018-10-26 05:48:20 -0700

    loaded bowsette image lol

---
+++ CassiniLecture/ImageViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ImageViewController.swift
+//  CassiniLecture
+//
+//  Created by Anti on 10/26/18.
+//  Copyright © 2018 Anti. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController
+{
+    var imageURL: URL? {
+        didSet {
+            imageView.image = nil
+            
+            // checks to see if it's on screen
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                imageView.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if imageURL == nil {
+            imageURL = DemoURLs.bowsette
+        }
+    }
+}
